cd4b906aad498fb3db46bf2bbb610054c3c131c7
AkanshuMittal <akanshumittal150@gmail.com>
2025-12-23 01:48:35 +0530

    Application

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -23,24 +23,23 @@ class PredictPipeline:
         except Exception as e:
             raise CustomException(e,sys)
         
-        class CustomData:
+class CustomData:
             def __init__( self,
                          gender: str,
                          race_ethnicity: str,
                          parental_level_of_education: str,
                          lunch: str,
                          test_preparation_course: str,
-                         reading_score: int,
-                         writing_score: int):
-    
-                
+                         math_score: int,
+                         reading_score: int):
+
                 self.gender = gender
                 self.race_ethnicity = race_ethnicity
                 self.parental_level_of_education = parental_level_of_education
                 self.lunch = lunch
                 self.test_preparation_course = test_preparation_course
+                self.math_score = math_score
                 self.reading_score = reading_score
-                self.writing_score = writing_score
 
             def get_data_as_data_frame(self):
                 try:
@@ -50,8 +49,8 @@ class PredictPipeline:
                         "parental_level_of_education": [self.parental_level_of_education],      
                         "lunch": [self.lunch],
                         "test_preparation_course": [self.test_preparation_course],
-                        "reading_score": [self.reading_score],
-                        "writing_score": [self.writing_score]
+                        "math_score": [self.math_score],
+                        "reading_score": [self.reading_score]
                     
                     }
 
